90abc3ac77c5af1c93b9f08d5b04a63e18b63254
Christopher Bryan <christopher_bryan@rocketmail.com>
2025-02-13 22:36:09 -0800

    added custom invalid token error when the JWT token that is passed has expired or is invalid

--- apps/auth/src/guards/jwt.guard.ts
+++ apps/auth/src/guards/jwt.guard.ts
@@ -1,5 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JsonWebTokenError } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  // TODO: auth gateway guard does not catch this invalid token exception and just return default error
+  handleRequest(err: any, user: any, info: any, context: any, status: any) {
+    if (info instanceof JsonWebTokenError) {
+      throw new UnauthorizedException('Invalid Token!');
+    }
+    console.log('info');
+    return super.handleRequest(err, user, info, context, status);
+  }
+}
